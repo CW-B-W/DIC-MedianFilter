1abd01cd62ab9f01d274776076f7287bb01ab3a6
CW-B-W <cw.brad.wang@gmail.com>
2021-05-28 20:54:27 +0800

    temp save MFE.v

--- MFE.v
+++ MFE.v
@@ -11,6 +11,32 @@ output reg [13:0] addr;
 output reg [ 7:0] data_wr;
 output reg        wen;
 
+parameter S0 = 0;
+reg [x:0] state;
+reg [x:0] n_state;
+
+always @(posedge clk or posedge rst) begin
+    state <= n_state;
+end
+
+always @(*) begin
+    if (reset) begin
+        
+    end
+    else begin
+        
+    end
+end
+
+always @(*) begin
+    if (reset) begin
+        
+        
+    end
+    else
+        
+    end
+end
 
 
 endmodule
